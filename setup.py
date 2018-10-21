685ff83b73dbef181b009697a58025aea506ecec
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2018-10-20 20:45:42 -0400

    Initialized application

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="facial-recognition",
+      version='1.0',
+      description="Simple facial recognition app using opencv",
+      author="Gurveer Dhindsa",
+      author_email="gurveerdhindsa@hotmail.com",
+      packages=['distutils','distutils.command','cv2'],
+     )
